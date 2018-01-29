f9a464afe43c21cfa0e9b97380af159c2ee522d1
Michelle Tanco <michelle.tanco@gmail.com>
2018-01-29 14:37:46 -0800

    Update run_and_interpret_results.sql
    Improved tabbing

--- run_and_interpret_results.sql
+++ run_and_interpret_results.sql
@@ -1,11 +1,11 @@
 --Table to hold our results
 DROP TABLE test_db.mt_kmeans_withinness;
 CREATE TABLE test_db.mt_kmeans_withinness (
-	run_id VARCHAR(40)				--for identifying runs
-	,number_of_clusters INT 		--k to be passed to the K-Means function
-	,cluster_id INT 				--a number between 1 and k inclusive
-	,cluster_size INT 				--number of items in each cluster
-	,withinness DECIMAL(13,4) 		--sum of squared error for each cluster
+	run_id VARCHAR(40)		--for identifying runs
+	,number_of_clusters INT 	--k to be passed to the K-Means function
+	,cluster_id INT 		--a number between 1 and k inclusive
+	,cluster_size INT 		--number of items in each cluster
+	,withinness DECIMAL(13,4) 	--sum of squared error for each cluster
 	,covergence_info VARCHAR(10000)	--output from kmeans
 ) NO PRIMARY INDEX;
 
@@ -46,16 +46,16 @@ LEFT OUTER JOIN TSES AS B
 	ON A.number_of_clusters = B.number_of_clusters + 1
 ORDER BY A.number_of_clusters
 
---run_id				number_of_clusters	total_withinness	improvement
---Initial Test K = 1	1					1959417.7424		null
---Initial Test K = 2	2					1154822.9925		0.5894
---Initial Test K = 3	3					779852.5520			0.6753
---Initial Test K = 4	4					351525.9869			0.4508
---Initial Test K = 5	5					303204.2756			0.8625
---Initial Test K = 6	6					284086.6847			0.9369
---Initial Test K = 7	7					263506.9589			0.9276
---Initial Test K = 8	8					245786.6623			0.9328
---Initial Test K = 9	9					263443.5268			1.0718
---Initial Test K = 10	10					204543.7955			0.7764
+--run_id		number_of_clusters	total_withinness	improvement
+--Initial Test K = 1	1			1959417.7424		null
+--Initial Test K = 2	2			1154822.9925		0.5894
+--Initial Test K = 3	3			779852.5520		0.6753
+--Initial Test K = 4	4			351525.9869		0.4508
+--Initial Test K = 5	5			303204.2756		0.8625
+--Initial Test K = 6	6			284086.6847		0.9369
+--Initial Test K = 7	7			263506.9589		0.9276
+--Initial Test K = 8	8			245786.6623		0.9328
+--Initial Test K = 9	9			263443.5268		1.0718
+--Initial Test K = 10	10			204543.7955		0.7764
 
 /*Four Clusters looks like the way to go*/
